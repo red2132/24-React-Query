ed6a28980ab92139a05a048302b146f037a116a9
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-06-23 14:25:00 +0900

    게시물 추가기능 추가

--- src/components/Events/EventForm.jsx
+++ src/components/Events/EventForm.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 
 import ImagePicker from '../ImagePicker.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchSelectableImages } from '../../util/http.js';
 
 export default function EventForm({ inputData, onSubmit, children }) {
   const [selectedImage, setSelectedImage] = useState(inputData?.image);
 
+  const {data, isPending, isError} = useQuery({
+    queryKey:['events-images'],
+    queryFn: fetchSelectableImages
+  })
   function handleSelectImage(image) {
     setSelectedImage(image);
   }
@@ -29,14 +35,22 @@ export default function EventForm({ inputData, onSubmit, children }) {
           defaultValue={inputData?.title ?? ''}
         />
       </p>
-
-      <div className="control">
-        <ImagePicker
-          images={[]}
-          onSelect={handleSelectImage}
-          selectedImage={selectedImage}
+      {isPending && <p>이미지 로딩 중...</p>}
+      {isError && 
+        <ErrorBlock 
+          title="문제 발생"
+          message={ error.info?.message || '이미지를 불러오지 못했습니다' }
         />
-      </div>
+      }
+      {data && (
+        <div className="control">
+          <ImagePicker
+            images={data}
+            onSelect={handleSelectImage}
+            selectedImage={selectedImage}
+          />
+        </div>
+      )}
 
       <p className="control">
         <label htmlFor="description">Description</label>
--- src/util/http.js
+++ src/util/http.js
@@ -1,18 +1,61 @@
-export async function fetchEvents({ signal, searchTerm }) {
-    let url = 'http://localhost:3000/events'
+import { QueryClient } from "@tanstack/react-query"
 
-    if(searchTerm) {
-      url += '?search=' + searchTerm
-    }
-    const response = await fetch(url, { signal: signal });
-  
-    if (!response.ok) {
-      const error = new Error('An error occurred while fetching the events')
-      error.code = response.status
-      error.info = await response.json()
-      throw error
-    }
-  
-    const { events } = await response.json()
-    return events;
+export const queryClient = new QueryClient()
+
+export async function fetchEvents({ signal, searchTerm }) {
+  let url = 'http://localhost:3000/events'
+
+  if (searchTerm) {
+    url += '?search=' + searchTerm
   }
+
+  const response = await fetch(url, { signal: signal })
+
+  if (!response.ok) {
+    const error = new Error('데이터 전송 중 문제가 발생했습니다')
+    error.code = response.status
+    error.info = await response.json()
+    throw error
+  }
+
+  const { events } = await response.json()
+
+  return events
+}
+
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: 'POST',
+    body: JSON.stringify(eventData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('저장 중에 문제가 발생했습니다')
+    error.code = response.status
+    error.info = await response.json()
+    throw error
+  }
+
+  const { event } = await response.json()
+
+  return event
+}
+
+export async function fetchSelectableImages({ signal }) {
+  const response = await fetch(`http://localhost:3000/events/images`, { signal })
+
+  if (!response.ok) {
+    const error = new Error('사진 업로드 중에 문제가 생겼습니다')
+    error.code = response.status
+    error.info = await response.json()
+    throw error
+  }
+
+  const { images } = await response.json()
+
+  return images
+}
